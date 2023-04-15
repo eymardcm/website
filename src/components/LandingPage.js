e095ad6d9b0b5742a5f1d4d68327e3f8b82ee8d9
Chad Eymard <chad.eymard@coop.org>
2023-04-15 19:08:43 -0400

    information blocks

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,6 +2,8 @@ import React from "react";
 import Lottie from "lottie-react";
 import {
   Button,
+  Card,
+  CardContent,
   Grid,
   Typography,
   makeStyles,
@@ -10,9 +12,11 @@ import {
 } from "@material-ui/core";
 import animationData from "../animations/landinganimation/data";
 import ButtonArrow from "./ui/ButtonArrow";
-import customSoftwareIcon from '../assets/Custom Software Icon.svg';
-import mobileAppsIcon from '../assets/mobileIcon.svg'
-import webSiteIcon from '../assets/websiteIcon.svg'
+import customSoftwareIcon from "../assets/Custom Software Icon.svg";
+import mobileAppsIcon from "../assets/mobileIcon.svg";
+import webSiteIcon from "../assets/websiteIcon.svg";
+import revolutionBackground from "../assets/repeatingBackground.svg";
+import infoBackground from "../assets/infoBackground.svg";
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -58,7 +62,7 @@ const useStyles = makeStyles((theme) => ({
     padding: 5,
     [theme.breakpoints.down("sm")]: {
       marginBottom: "2em",
-    }
+    },
   },
   mainContainer: {
     marginTop: "5em",
@@ -87,13 +91,43 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: "2em",
     [theme.breakpoints.down("xs")]: {
       marginLeft: 0,
-    }
+    },
   },
   servicesContainer: {
     marginTop: "12em",
     [theme.breakpoints.down("sm")]: {
-      padding: 25
-    }
+      padding: 25,
+    },
+  },
+  revolutionBackground: {
+    backgroundImage: `url(${revolutionBackground})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%",
+  },
+  revolutionCard: {
+    position: "absolute",
+    boxShadow: theme.shadows[10],
+    borderRadius: 15,
+    padding: "10em",
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: "8em",
+      paddingBottom: "8em",
+      paddingLeft: 0,
+      paddingRight: 0,
+      borderRadius: 0,
+      width: "100%",
+    },
+  },
+  infoBackground: {
+    backgroundImage: `url(${infoBackground})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%",
   },
 }));
 
@@ -101,10 +135,13 @@ export default function LandingPage() {
   const classes = useStyles();
   const theme = useTheme();
   const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+  const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
 
   return (
     <Grid container direction="column" className={classes.mainContainer}>
-      <Grid item> {/* ----- Hero Block ----- */}
+      <Grid item>
+        {" "}
+        {/* ----- Hero Block ----- */}
         <Grid
           container
           direction="row"
@@ -139,74 +176,252 @@ export default function LandingPage() {
             </Grid>
           </Grid>
           <Grid sm item className={classes.animation}>
-            <Lottie animationData={animationData} loop={true} />
+            <Lottie animationData={animationData} loop={false} />
           </Grid>
         </Grid>
       </Grid>
-      <Grid item> {/* ----- Custom Software Block ----- */}
-        <Grid container direction="row" justifyContent={matchesSM ? "center" : undefined } className={classes.servicesContainer}>
-          <Grid item style={{ marginLeft: matchesSM ? 0: "5em", textAlign: matchesSM ? "center": undefined }}>
-            <Typography variant="h4">
-              Custom Software Development
-            </Typography>
+      <Grid item>
+        {" "}
+        {/* ----- iOS/Android Block ----- */}
+        <Grid
+          container
+          direction="row"
+          justifyContent={matchesSM ? "center" : undefined}
+          className={classes.servicesContainer}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : "5em",
+              textAlign: matchesSM ? "center" : undefined,
+            }}
+          >
+            <Typography variant="h4">Custom Software Development</Typography>
             <Typography variant="subtitle1" className={classes.subtitle}>
-                Save Energy. Save Time. Save Money.
+              Save Energy. Save Time. Save Money.
             </Typography>
             <Typography variant="subtitle1">
-                Complete digital solutions, from investigation to{" "}<span className={classes.specialText}>celebration.</span>.
+              Complete digital solutions, from investigation to{" "}
+              <span className={classes.specialText}>celebration.</span>.
             </Typography>
             <Button variant="outlined" className={classes.learnButton}>
-                <span style={{ marginRight: 10 }}>Learn More</span>
-                <ButtonArrow width={10} height={10} fill={theme.palette.common.blue} />
+              <span style={{ marginRight: 10 }}>Learn More</span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
             </Button>
           </Grid>
           <Grid item>
-            <img alt="customer software icon" src={customSoftwareIcon} className={classes.icon}/>
+            <img
+              alt="customer software icon"
+              src={customSoftwareIcon}
+              className={classes.icon}
+            />
           </Grid>
         </Grid>
       </Grid>
-      <Grid item> {/* ----- iOS/Android Block ----- */}
-        <Grid container direction="row" justifyContent={matchesSM ? "center" : "flex-end" } className={classes.servicesContainer}>
-          <Grid item style={{ marginLeft: matchesSM ? 0: "5em", textAlign: matchesSM ? "center": undefined }}>
-            <Typography variant="h4">
-              iOS/Android App Development
-            </Typography>
+      <Grid item>
+        {" "}
+        {/* ----- iOS/Android Block ----- */}
+        <Grid
+          container
+          direction="row"
+          justifyContent={matchesSM ? "center" : "flex-end"}
+          className={classes.servicesContainer}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : "5em",
+              textAlign: matchesSM ? "center" : undefined,
+            }}
+          >
+            <Typography variant="h4">iOS/Android App Development</Typography>
             <Typography variant="subtitle1" className={classes.subtitle}>
-                Extend Functionality. Extend Access. Extend Engagement.
+              Extend Functionality. Extend Access. Extend Engagement.
             </Typography>
             <Typography variant="subtitle1">
-                Integrate your web experience or create a standalone app{matchesSM ? null : <br />} with either mobile platform.
+              Integrate your web experience or create a standalone app
+              {matchesSM ? null : <br />} with either mobile platform.
             </Typography>
             <Button variant="outlined" className={classes.learnButton}>
-                <span style={{ marginRight: 10 }}>Learn More</span>
-                <ButtonArrow width={10} height={10} fill={theme.palette.common.blue} />
+              <span style={{ marginRight: 10 }}>Learn More</span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
             </Button>
           </Grid>
-          <Grid item style={{ marginRight: matchesSM ? 0: "5em", textAlign: matchesSM ? "center": undefined }}>
-            <img alt="mobile phone icon" src={mobileAppsIcon} className={classes.icon}/>
+          <Grid
+            item
+            style={{
+              marginRight: matchesSM ? 0 : "5em",
+              textAlign: matchesSM ? "center" : undefined,
+            }}
+          >
+            <img
+              alt="mobile phone icon"
+              src={mobileAppsIcon}
+              className={classes.icon}
+            />
           </Grid>
         </Grid>
       </Grid>
-      <Grid item> {/* ----- Website Block ----- */}
-        <Grid container direction="row" justifyContent={matchesSM ? "center" : undefined } className={classes.servicesContainer}>
-          <Grid item style={{ marginLeft: matchesSM ? 0: "5em", textAlign: matchesSM ? "center": undefined }}>
-            <Typography variant="h4">
-              Website Development
-            </Typography>
+      <Grid item>
+        {" "}
+        {/* ----- Website Block ----- */}
+        <Grid
+          container
+          direction="row"
+          justifyContent={matchesSM ? "center" : undefined}
+          className={classes.servicesContainer}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : "5em",
+              textAlign: matchesSM ? "center" : undefined,
+            }}
+          >
+            <Typography variant="h4">Website Development</Typography>
             <Typography variant="subtitle1" className={classes.subtitle}>
-                Reach More. Discover More. Sell More.
+              Reach More. Discover More. Sell More.
             </Typography>
             <Typography variant="subtitle1">
-                Optimized for search engines, built for speed.
+              Optimized for search engines, built for speed.
             </Typography>
             <Button variant="outlined" className={classes.learnButton}>
-                <span style={{ marginRight: 10 }}>Learn More</span>
-                <ButtonArrow width={10} height={10} fill={theme.palette.common.blue} />
+              <span style={{ marginRight: 10 }}>Learn More</span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
             </Button>
           </Grid>
           <Grid item>
-            <img alt="website icon" src={webSiteIcon} className={classes.icon}/>
+            <img
+              alt="website icon"
+              src={webSiteIcon}
+              className={classes.icon}
+            />
           </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        {" "}
+        {/* ----- The Revolution Block ----- */}
+        <Grid
+          container
+          style={{ height: "100em", marginTop: "12em" }}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Card className={classes.revolutionCard}>
+            <CardContent>
+              <Grid
+                container
+                direction="column"
+                style={{ textAlign: "center" }}
+              >
+                <Grid item>
+                  <Typography variant="h3" gutterBottom>
+                    The Revolution
+                  </Typography>
+                  <Typography variant="subtitle1">
+                    Visionary insights coupled with cutting-edge technology is a
+                    recipe for revolution.
+                  </Typography>
+                  <Button
+                    className={classes.learnButtonHero}
+                    variant="outlined"
+                  >
+                    <span style={{ marginRight: 10 }}>Learn More</span>
+                    <ButtonArrow
+                      width={15}
+                      height={15}
+                      fill={theme.palette.common.blue}
+                    />
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+          <div className={classes.revolutionBackground} />
+        </Grid>
+      </Grid>
+      <Grid item>
+        {" "}
+        {/* ----- Information Block ----- */}
+        <Grid
+          container
+          direction="row"
+          style={{ height: "80em" }}
+          alignItems="center"
+        >
+          <Grid
+            item
+            container
+            style={{
+              position: "absolute",
+              textAlign: matchesXS ? "center" : "inherit",
+            }}
+            direction={matchesXS ? "column" : "row"}
+            spacing={matchesXS ? 10 : 0}
+          >
+            <Grid
+              item
+              sm
+              style={{ marginLeft: matchesXS ? 0 : matchesSM ? "2em" : "5em" }}
+            >
+              <Grid container direction="column">
+                <Typography variant="h2" style={{ color: "white" }}>
+                  About Us
+                </Typography>
+                <Typography variant="subtitle2">Let's get personal.</Typography>
+                <Grid item>
+                  <Button
+                    variant="outlined"
+                    style={{ color: "white", borderColor: "white" }}
+                    className={classes.learnButton}
+                  >
+                    <span style={{ marginRight: 10 }}>Learn More</span>
+                    <ButtonArrow width={10} height={10} fill={"white"} />
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              sm
+              style={{
+                marginRight: matchesXS ? 0 : matchesSM ? "2em" : "5em",
+                textAlign: matchesXS ? "center" : "right",
+              }}
+            >
+              <Grid container direction="column">
+                <Typography variant="h2" style={{ color: "white" }}>
+                  Contact Us
+                </Typography>
+                <Typography variant="subtitle2">Say hello!</Typography>
+                <Grid item>
+                  <Button
+                    variant="outlined"
+                    style={{ color: "white", borderColor: "white" }}
+                    className={classes.learnButton}
+                  >
+                    <span style={{ marginRight: 10 }}>Learn More</span>
+                    <ButtonArrow width={10} height={10} fill={"white"} />
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <div className={classes.infoBackground} />
         </Grid>
       </Grid>
     </Grid>
